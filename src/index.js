8de9cdbb5e1f9164246b78e0e0fbaa30429eed35
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2018-11-06 18:41:56 +0530

    viedoList

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY = 'AIzaSyCkJb5c66i3oDLLcKOV49ZwJbfSN75gqhg'
+const API_KEY = 'AIzaSyCkJb5c66i3oDLLcKOV49ZwJbfSN75gqhg';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' },  (data) => {
+      this.setState({ videos: data});
+    });
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
